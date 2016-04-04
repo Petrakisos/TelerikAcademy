b9718b5f12b7eb79506569a24cce2c20426874b8
Petrakios <pepy.hristov@gmail.com>
2016-04-05 00:16:46 +0300

    Added Age and DateAndTime.cs

---
+++ 01IntroProgrammingHomework/Age/Age.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Age
+{
+    static void Main()
+    {
+        string enteredDate = Console.ReadLine();
+        DateTime myDate = DateTime.ParseExact(enteredDate, "mm.dd.yyyy", null);
+        DateTime now = DateTime.Now;
+        int Age = (int)((now - myDate).TotalDays / 365.242199);
+        Console.WriteLine(Age);
+        Console.WriteLine(Age +10);
+    }
+}
