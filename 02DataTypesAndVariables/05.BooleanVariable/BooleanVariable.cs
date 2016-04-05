e3653d7046838568fab96f0ab27eb4072621f76c
Petrakios <pepy.hristov@gmail.com>
2016-04-05 19:43:03 +0300

    Added BooleanVariable.cs

---
+++ 02DataTypesAndVariables/05.BooleanVariable/BooleanVariable.cs
@@ -0,0 +1,22 @@
+﻿//Boolean Variable
+//Description
+//Declare a Boolean variable called isFemale and assign an appropriate value corresponding to your gender.Print it on the console.
+
+using System;
+
+    class BooleanVariable
+{
+    static void Main()
+    {
+        bool isFemale = false; //boolean assing value
+        Console.WriteLine(isFemale); // print
+///////////////////////////////////////////////////////////
+        string value = "False"; //  var string
+        bool isFemale2 = bool.Parse(value); // declare bool and parse value. Value can assing other values --> value = "True"
+        Console.WriteLine(value);   // 1
+
+        value = "True";
+        isFemale2 = bool.Parse(value);
+        Console.WriteLine(value); // 2
+    }
+}
