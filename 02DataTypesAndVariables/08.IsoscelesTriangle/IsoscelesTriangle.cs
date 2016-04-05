210f0e152719f34beb5cae6ca76a3f971da92344
Petrakios <pepy.hristov@gmail.com>
2016-04-05 21:15:56 +0300

    Added IsoscelesTriangle.cs

---
+++ 02DataTypesAndVariables/08.IsoscelesTriangle/IsoscelesTriangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class IsoscelesTriangle
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+/////////////////////////////////////////////////////////////////////////////////////////// 1
+        char copyRight = '©';
+        Console.WriteLine("{0,4}\n{0,3}{0,2}\n{0,2}{0,4}\n{0}{0,2}{0,2}{0,2}", copyRight);
+////////////////////////////////////////////////////////////////////////////////////////// 2
+        char COPYRIGHT = '©';
+        Console.WriteLine(new String(' ', 5) + COPYRIGHT);
+        Console.WriteLine(new String(' ', 4) + COPYRIGHT + new String(' ', 1) + COPYRIGHT);
+        Console.WriteLine(new String(' ', 3) + COPYRIGHT + new String(' ', 3) + COPYRIGHT);
+        Console.WriteLine(new String(' ', 2) + COPYRIGHT + new String(' ', 5) + COPYRIGHT);
+        Console.WriteLine(new String(' ', 1) + COPYRIGHT + new String(' ', 7) + COPYRIGHT);
+        Console.WriteLine(" " + new String(COPYRIGHT, 9));
+////////////////////////////////////////////////////////////////////////////////////////// 3
+        int n = 7;
+
+        for (int i = 1; i <= n; i += 2)
+        {
+            for (int k = (n - i) / 2; k >= 0; k--)
+                Console.Write(" ");
+            for (int j = 1; j <= i; j++)
+                Console.Write("©");
+            Console.WriteLine();
+        }
+    }
+}
