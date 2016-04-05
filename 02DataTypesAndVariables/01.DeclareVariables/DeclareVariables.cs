6d3a9a7d76eedc59e146eb011caa804c8899e8a3
Petrakisos <pepy.hristov@gmail.com>
2016-04-05 16:35:38 +0300

    Added FloatOrDouble.cs and added comments in the code

--- 02DataTypesAndVariables/01.DeclareVariables/DeclareVariables.cs
+++ 02DataTypesAndVariables/01.DeclareVariables/DeclareVariables.cs
@@ -10,27 +10,27 @@ class DeclareVarialbes
 {
     static void Main()
     {
-        short num1; //-10000
-        sbyte num2; //-115
-        byte num3;  //97
-        ushort num4;    //52130    
-        int num5;   //4825932
+        short num1; //-10000 Declarate varible
+        sbyte num2; //-115 Declarate varible
+        byte num3;  //97 Declarate varible
+        ushort num4;    //52130 Declarate varible
+        int num5;   //4825932 Declarate varible
 
-        num1 = -10000;
+        num1 = -10000; // give it value
         Console.WriteLine(num1);
-        num2 = -115;
+        num2 = -115;    // give it value
         Console.WriteLine(num2);
-        num3 = 97;
+        num3 = 97;  // give it value
         Console.WriteLine(num3);
-        num4 = 52130;
+        num4 = 52130;   // give it value
         Console.WriteLine(num4);
-        num5 = 4825932;
+        num5 = 4825932; // give it value
         Console.WriteLine(num5);
-        num1 = 1;
-        num2 = 2;
-        num3 = 3;
-        num4 = 4;
-        num5 = 5;
+        num1 = 1;   // value changed
+        num2 = 2;   // value changed
+        num3 = 3;   // value changed
+        num4 = 4;   // value changed
+        num5 = 5;   // value changed
         Console.WriteLine("{0} {1} {2} {3} {4}", num1, num2, num3, num4, num5);
     }
 }
---
+++ 02DataTypesAndVariables/02.FloatOrDouble/FloatOrDouble.cs
@@ -0,0 +1,31 @@
+﻿//Float or Double
+//Description
+//Which of the following values can be assigned to a variable of type float and which to a variable 
+//of type double: 34.567839023, 12.345, 8923.1234857, 3456.091? Write a program to assign the numbers 
+//in variables and print them to ensure no precision is lost.
+using System;
+
+class FloatOrDouble
+{
+    static void Main()
+    {
+        float num1; //12.345
+        float num2; //3456.091
+        double num3;    //8923.1234857
+        double num4;    //34.567839023
+
+        num1 = 12.345f; //give value for varialbe
+        Console.WriteLine(num1);
+        num2 = 3456.091f;   //give value for varialbe
+        Console.WriteLine(num2);
+        num3 = 8923.1234857f;   //give value for varialbe
+        Console.WriteLine(num3);
+        num4 = 34.567839023;    //give value for varialbe
+        Console.WriteLine(num4);
+        num1 = 1.111111f; // changed varialbe value
+        num2 = 2.222222f;   // changed varialbe value
+        num3 = 3.33333333333333;    // changed varialbe value
+        num4 = 4.44444444444444;    // changed varialbe value
+        Console.WriteLine("{0} {1} {2} {3}" , num1, num2, num3, num4);
+    }
+}
