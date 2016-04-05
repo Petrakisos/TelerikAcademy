f7f2f15cdf7d86d1c7e5edaba647349cadbbc580
Petrakisos <pepy.hristov@gmail.com>
2016-04-05 16:14:59 +0300

    Added DeclareVariables.cs

--- 02DataTypesAndVariables/01.DeclareVariables/DeclareVariables.cs
+++ 02DataTypesAndVariables/01.DeclareVariables/DeclareVariables.cs
@@ -6,10 +6,32 @@
 //You do not have to submit anything for this problem
 using System;
 
-class DeclareVariables
+class DeclareVarialbes
 {
     static void Main()
     {
+        short num1; //-10000
+        sbyte num2; //-115
+        byte num3;  //97
+        ushort num4;    //52130    
+        int num5;   //4825932
 
+        num1 = -10000;
+        Console.WriteLine(num1);
+        num2 = -115;
+        Console.WriteLine(num2);
+        num3 = 97;
+        Console.WriteLine(num3);
+        num4 = 52130;
+        Console.WriteLine(num4);
+        num5 = 4825932;
+        Console.WriteLine(num5);
+        num1 = 1;
+        num2 = 2;
+        num3 = 3;
+        num4 = 4;
+        num5 = 5;
+        Console.WriteLine("{0} {1} {2} {3} {4}", num1, num2, num3, num4, num5);
     }
 }
+
