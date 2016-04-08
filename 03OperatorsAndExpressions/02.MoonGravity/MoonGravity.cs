0999527426d19ec3b11e06e6fa373d29a7739388
Petrakios <pepy.hristov@gmail.com>
2016-04-09 00:21:59 +0300

    Added MoonGravity.cs

---
+++ 03OperatorsAndExpressions/02.MoonGravity/MoonGravity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class MoonGravity
+{
+    static void Main()
+    {
+        double w = double.Parse(Console.ReadLine());
+        double moonGravityValue = 17;
+        double result = (w * moonGravityValue) / 100;
+        Console.WriteLine("{0:0.000}", result);
+    }
+}
