a0082d272fd11211697d7cfb39170bc196583d5a
Petrakios <pepy.hristov@gmail.com>
2016-04-19 18:37:44 +0300

    C# added Loops - NumberFrom1ToN.cs NotDivisableNumbers and MMSAOfNNumbers.cs-isn't ready HTML - FormsAndFrames - RegistrationForm

---
+++ CSharpPart1/06.Loops/02.NotDivisibleNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 3;
+        int b = 7;
+
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++)
+        {
+            if ( ((i % a) != 0) && ((i % b) != 0))
+            {
+                Console.Write("{0} ", i);
+            }
+        }
+    }
+}
---
+++ CSharpPart1/06.Loops/03.MMSAOfNNumbers/MMSAOfNNumbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class MMSAOfNNumbers
+{
+    static void Main()
+    {
+        int min = int.MinValue;
+        int max = int.MaxValue;
+        int sum;
+        int avg;
+
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.ReadLine();
+        }
+    }
+}
