15eaa0da46405d5209b20f7923a47ba40792938e
Petrakisos <pepy.hristov@gmail.com>
2016-04-22 17:27:29 +0300

    Added CalculateAgain and Calculate3

---
+++ CSharpPart1/06.Loops/06.CalculateAgain/CalculateAgain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class CalculateAgain
+{
+    static void Main()
+    {
+        decimal n = decimal.Parse(Console.ReadLine());
+        decimal k = decimal.Parse(Console.ReadLine());
+
+        decimal sum = 1;
+
+        for (decimal i = k + 1; i <= n; i++)
+        {
+            sum *= i;
+        }
+        Console.WriteLine(sum);
+    }
+}
---
+++ CSharpPart1/06.Loops/07.Calculate3/Calculate3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Numerics;
+
+class Calculate3
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int k = int.Parse(Console.ReadLine());
+
+        BigInteger nFactorial = 1;
+        BigInteger kFactorial = 1;
+        BigInteger product = 0;
+        BigInteger factorielProduct = 1;
+
+        for (int i = 1; i < n + 1; i++)
+        {
+            nFactorial *= i;
+
+            if (i <= k)
+            {
+                kFactorial *= i;
+            }
+        }
+
+        for (int j = n - k; j > 1; j--)
+        {
+            factorielProduct *= j;
+        }
+
+        product = (nFactorial / (kFactorial * factorielProduct));
+        Console.WriteLine(product);
+    }
+}
