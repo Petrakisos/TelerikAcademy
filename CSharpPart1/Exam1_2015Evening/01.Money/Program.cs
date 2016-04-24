01619260822b574429a4a46db986e496e367e0a6
Petrakios <pepy.hristov@gmail.com>
2016-04-24 20:12:56 +0300

    Added few solved exams - Money, EncodingSum.cs, Printing, TextToNumber.cs

---
+++ CSharpPart1/Exam1_2015Evening/01.Money/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // read input
+        int numberOfStudents = int.Parse(Console.ReadLine());
+        int sheetsPerStudent = int.Parse(Console.ReadLine());
+        double realmPrice = double.Parse(Console.ReadLine());
+
+        double realmSize = 400;
+
+        // logic
+        double savedMoney = (numberOfStudents * sheetsPerStudent) / realmSize * realmPrice;
+        // print result
+        Console.WriteLine("{0:F3}", savedMoney);
+    }
+}
---
+++ CSharpPart1/Exam1_2015Morning/01.Printing/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // read input
+        int numOfStudents = int.Parse(Console.ReadLine());
+        int paperPerStudent = int.Parse(Console.ReadLine());
+        decimal realmPrice = decimal.Parse(Console.ReadLine());
+
+        decimal realmSize = 500.00M;
+
+        // logic
+        decimal savedMoney = ((numOfStudents * paperPerStudent) / realmSize) * realmPrice;
+
+        // print result
+        Console.WriteLine("{0:F2}", savedMoney);
+
+    }
+}
