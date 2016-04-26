ea42312b93b99e3518d69838a10dbf4cad3d1d0b
Petrakisos <pepy.hristov@gmail.com>
2016-04-26 15:57:22 +0300

    Added few exams Niki

---
+++ CSharpPart1/Exams2015/03.ConsoleApplication1 - Niki/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        int index = 0; // reads the index of the digit
+        BigInteger globalProduct = 1;
+        while (true)
+        {
+            string line = Console.ReadLine();
+            
+            if (line == "END")
+            {
+                break;
+            }
+
+            if (index == 10)
+            {
+                Console.WriteLine(globalProduct);
+                globalProduct = 1;
+            }
+
+            long num = long.Parse(line); // read the digit
+            if (index % 2 == 1) // if the position is on odd position
+            {
+                long product = 1;
+                while (num > 0)
+                {
+                    long digit = num % 10; // take the last digit in num
+                    if (digit != 0)
+                    {
+                        product *= digit;
+                    }
+                    num /= 10;
+                }
+                globalProduct *= product;
+            }
+            index++;
+        }
+        Console.WriteLine(globalProduct);
+    }
+}
---
+++ CSharpPart1/Exams2015/04.PersianRugs - Niki/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+        int D = int.Parse(Console.ReadLine());
+        int width = 2 * N + 1;
+
+        // loop for top
+        for (int row = 0; row < N; row++)
+        {
+            Console.Write(new string('#', row)); // upper left
+            Console.Write('\\'); // upper left
+
+            int spaceLeft = width - row - row - 1 - 1 - D - D;
+            if (spaceLeft >= 2)
+            {
+                Console.Write(new string(' ', D));
+                Console.Write('\\');
+                Console.Write(new string('.', spaceLeft -2));
+                Console.Write('/');
+                Console.Write(new string(' ', D));
+                
+            }
+            else
+            {
+                Console.Write(new string(' ', width - row - row - 1 - 1));
+            }
+            Console.Write('/'); // upper right
+            Console.Write(new string('#', row)); // upper right
+
+
+
+            Console.WriteLine();
+        }
+
+        Console.WriteLine(new string('#', N) + 'X' + new string('#', N)); // middle row
+
+        // loop for bottom
+        for (int row = N - 1; row >= 0; row--)
+        {
+            Console.Write(new string('#', row)); // bottom left
+            Console.Write('/'); // bottom left
+
+            int spaceLeft = width - row - row - 1 - 1 - D - D;
+            if (spaceLeft >= 2)
+            {
+                Console.Write(new string(' ', D));
+                Console.Write('/');
+                Console.Write(new string('.', spaceLeft - 2));
+                Console.Write('\\');
+                Console.Write(new string(' ', D));
+
+            }
+            else
+            {
+                Console.Write(new string(' ', width - row - row - 1 - 1));
+            }
+
+            Console.Write('\\'); // bottom right
+            Console.Write(new string('#', row)); // bottom right
+
+            Console.WriteLine();
+        }
+    }
+}
