5110632b72d92fd1bf5486c4eb1f65bcd640f99f
Petrakios <pepy.hristov@gmail.com>
2016-05-03 13:15:21 +0300

    Added C#2 homework Arrays - 01.AlocateArray

---
+++ CSharpPart2/01.Arrays/01.AllocateArray/AllocateArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.AllocateArray
+{
+    class AllocateArray
+    {
+        static void Main()
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            int multiply = 5;
+
+            int[] array = new int[N]; // create array with N size
+
+            for (int i = 0; i < array.Length; i++) // loop array size
+            {
+                array[i] = i * multiply; // take each index from 0 to N and multiply it by 5
+                Console.WriteLine(array[i]); // write line result for each index
+            }
+        }
+    }
+}
