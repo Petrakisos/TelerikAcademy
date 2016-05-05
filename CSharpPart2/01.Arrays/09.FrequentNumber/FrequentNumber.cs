6d6f4ff06df2f8a9b79acde13a0478959f876221
Petrakios <pepy.hristov@gmail.com>
2016-05-05 16:20:32 +0300

    Sloved 09 FrequentNumber.cs

--- CSharpPart2/01.Arrays/09.FrequentNumber/FrequentNumber.cs
+++ CSharpPart2/01.Arrays/09.FrequentNumber/FrequentNumber.cs
@@ -4,6 +4,35 @@ class FrequentNumber
 {
     static void Main()
     {
+        int N = int.Parse(Console.ReadLine());
 
+        int[] array = new int[N]; // {4, 1, 1, 4, 2, 3, 4, 4, 1, 2, 4, 9, 3};
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = int.Parse(Console.ReadLine());
+        }
+        int bestRepeats = 0;
+        int bestNumber = 0;
+        int repeats = 0;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            repeats = 0;
+
+            for (int j = 0; j < array.Length; j++)
+            {
+                if (array[i] == array[j])
+                {
+                    repeats++;
+                }
+                if (repeats > bestRepeats)
+                {
+                    bestRepeats = repeats;
+                    bestNumber = array[i];
+                }
+            }
+        }
+        Console.WriteLine("{0} ({1} times)", bestNumber, bestRepeats);
     }
 }
