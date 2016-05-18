fa8c6a290e309d8939c339edc75ebf9aacdbb3b7
Petrakios <pepy.hristov@gmail.com>
2016-05-18 19:36:12 +0300

    Added HW UsingClassesAndObjects

---
+++ CSharpPart2/05.UsingClassesAndObjects/01.LeapYear/LeapYear.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class LeapYear
+{
+    private static void CheckIsLeapYear(int year)
+    {
+        if (DateTime.IsLeapYear(year))
+        {
+            Console.WriteLine("Leap");
+        }
+        else
+        {
+            Console.WriteLine("Common");
+        }
+    }
+
+    static void Main()
+    {
+        int year = int.Parse(Console.ReadLine());
+        CheckIsLeapYear(year);
+    }
+
+
+}
