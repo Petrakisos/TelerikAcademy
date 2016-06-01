9d4b3cc37afde44ad9ffb93e21e79c1cd36d67c2
Petrakisos <pepy.hristov@gmail.com>
2016-06-01 14:29:40 +0300

    ExamPrep Problems

---
+++ CSharpPart2/ExamPrep/02.MovingLetters - Ivaylo/MovingLetters.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovingLettersIvaylo
+{
+    class MovingLetters
+    {
+        static StringBuilder ExtractLetters(string[] words)
+        {
+            StringBuilder result = new StringBuilder();
+            int maxWordLenght = 0;
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                string currentWord = words[i];
+                if (maxWordLenght < currentWord.Length)
+                {
+                    maxWordLenght = currentWord.Length;
+                }
+            }
+
+            for (int i = 0; i < maxWordLenght; i++)
+            {
+                for (int j = 0; j < words.Length; j++)
+                {
+                    string currentWord = words[j];
+
+                    if (i < currentWord.Length)
+                    {
+                        int lastLetter = - 1 - i;
+                        result.Append(currentWord[lastLetter]);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        static string MoveLetters(StringBuilder stragePieceOfWords)
+        {
+            for (int i = 0; i < stragePieceOfWords.Length; i++)
+            {
+                char currentSymbol = stragePieceOfWords[i];
+                int transition = char.ToLower(currentSymbol) - 'a' + 1;
+
+                int nextPosition = (i + transition) % stragePieceOfWords.Length;
+                stragePieceOfWords.Remove(i, 1);
+                stragePieceOfWords.Insert(nextPosition, currentSymbol);
+            }
+
+            return stragePieceOfWords.ToString();
+        }
+
+        static void Main()
+        {
+            string[] words = Console.ReadLine().Split(' ');
+
+            StringBuilder strangeCombinationOfLetters = ExtractLetters(words);
+            string finalResult = MoveLetters(strangeCombinationOfLetters);
+
+            Console.WriteLine(finalResult);
+        }
+    }
+}
---
+++ CSharpPart2/ExamPrep/1.DeCatCoding - Koce/DeCatCoding.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeCatCodingKoce
+{
+    class DeCatCoding
+    {
+        // 1. read input
+        // 1.1 extract all cat numbers from the input
+        // 2. convert all cat numbers to decimal
+        // 3. convert all decimal numbers to base 26
+        // 4. join by space and print
+
+        static ulong CatToDec(string catNumber)
+        {
+            ulong result = 0;
+
+            foreach (char digit in catNumber)
+            {
+                result = (ulong)(digit - 'a') + result * 21;    
+            } 
+
+            return result;
+        }
+
+        static string DecTo26(ulong dec)
+        {
+            string result = string.Empty;
+
+            do
+            {
+                char digitValue = (char)('a' + (dec % 26));
+                result = digitValue + result;
+                dec /= 26;
+            }
+            while (dec > 0);
+
+            return result;
+        }
+
+        static void Main()
+        {
+            string[] numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(CatToDec)
+                .Select(DecTo26)
+                .ToArray();
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
